8aa6f64e6c7c4733d475ab1bfd0495790dce96d3
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-04-07 15:23:05 +0700

    Feat: Login register with google and github

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,9 +4,13 @@ import Image from 'next/image';
 import React from 'react'
 import imgAvt from "../../public/images/placeholder.jpg"
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined; 
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
-    <Image className="rounded-full" height="30" width="30" alt="Avatar" src={imgAvt} />
+    <Image className="rounded-full" height="30" width="30" alt="Avatar" src={src || imgAvt} />
   )
 }
 
