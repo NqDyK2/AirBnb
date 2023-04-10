ce86d3126db0f4220eff7af83481db061962da14
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-04-10 14:26:04 +0700

    Feat: Listing creation step 1 3:37:38

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -18,9 +18,11 @@ import Input from '../inputs/Input'
 import { toast } from "react-hot-toast"
 import Button from '../Button'
 import { signIn } from 'next-auth/react'
+import useLoginModal from '@/app/hooks/useLoginModal'
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
     const { register, handleSubmit, formState: {
         errors,
@@ -44,6 +46,11 @@ const RegisterModal = () => {
                 setIsLoading(false)
             })
     }
+
+    const toggle = useCallback(() => {
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [loginModal, registerModal])
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -96,7 +103,7 @@ const RegisterModal = () => {
                     <div>
                         Already have an account?
                     </div>
-                    <div onClick={registerModal.onClose} className='text-neutral-800 cursor-pointer hover:underline'>
+                    <div onClick={toggle} className='text-neutral-800 cursor-pointer hover:underline'>
                         Log in
                     </div>
                 </div>
