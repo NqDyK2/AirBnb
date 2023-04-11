4ed0c9315114fbd4d530d58525fd952660db55f1
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-04-11 11:01:49 +0700

    Feat: Done Listing creation step 2

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import { Field, FieldValue, FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../inputs/CountrySelect'
+import Map from '../Map'
+import dynamic from 'next/dynamic'
 
 enum STEPS {
     CATEGORY = 0,
@@ -44,6 +46,9 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }),[location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -107,6 +112,9 @@ const RentModal = () => {
                 <CountrySelect
                     value={location}
                     onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map 
+                    center={location?.latlng}
                 />
             </div>
         )
