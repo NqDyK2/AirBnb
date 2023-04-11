f71fb79f33184988cc484a4fa396d77e3d5c7411
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-04-11 15:44:06 +0700

    Feat: done listing creation step 4 (Image upload, Cloudinary CDN)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -51,6 +51,7 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc')
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -164,7 +165,10 @@ const RentModal = () => {
                     title='Add a photo of your place'
                     subtitle='Show guests what your place looks like!'
                 />
-                <ImageUpload />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue("imageSrc", value)}
+                />
             </div>
         )
     }
