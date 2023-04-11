7ce8669fa553763e72633d31c89b6af0bc826a1c
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-04-11 17:47:40 +0700

    Feat: done Listing creation step 5 (Description and Price, Listing creation POST route)

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -27,7 +27,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     return (
         <CldUploadWidget
             onUpload={handleUpload}
-            uploadPreset={uploadPreset}
+            uploadPreset="b1yuxfnb"
             options={{
                 maxFiles: 1
             }}
